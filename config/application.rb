b645165c53412bbd5305d96548d29eed3d35ade1
chaad98 <faritzchad@yahoo.com>
2023-10-04 19:09:19 +0800

    Ready to push to the server

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module ALGODLA
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.hosts << "algodla.com"
+    config.hosts << "www.algodla.com"
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
