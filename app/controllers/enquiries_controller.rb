356d9f3c57131b469d79acbb97753b077e59eb3b
chaad98 <faritzchad@yahoo.com>
2023-10-10 11:51:45 +0800

    Add connection to external API

--- app/controllers/enquiries_controller.rb
+++ app/controllers/enquiries_controller.rb
@@ -1,3 +1,5 @@
+require 'httparty'
+
 class EnquiriesController < ApplicationController
     def index
         @enquiries = Enquiry.all
@@ -12,11 +14,30 @@ class EnquiriesController < ApplicationController
 
     def create
         @enquiry = Enquiry.new(enquiry_params)
-
+        
         if @enquiry.save
-            redirect_to root_path, success: "Form has been submitted successfully!"
+            # Save the data to the JSON file after a new record is created
+            save_to_json
+
+            # Build the JSON data you want to send to the external API
+            json_data = @enquiry.attributes.to_json
+
+            # Build the JSON data you want to send to the external API
+            api_url = 'https://www.api-controller.com/api/algodla_contact_form_mail.php?json='
+            api_url_with_query = "#{api_url}#{URI.encode_www_form_component(json_data)}"
+
+            # Send the JSON data to the external API using HTTParty
+            response = HTTParty.post(api_url_with_query, body: json_data, headers: { 'Content-Type' => 'application/json' })
+
+            if response.success?
+                redirect_to root_path, success: "Forms has been submitted successfully!"
+                Rails.logger.info("API Response: #{response.code} - #{response.body}")
+            else
+                Rails.logger.error("API Error: #{response.code} - #{response.body}")
+                flash.now[:danger] = "Error sending data to the API!"
+            end
         else
-            render :new
+            redirect_to root_path, danger: "Please key in all of the details!"
         end
     end
 
@@ -25,10 +46,18 @@ class EnquiriesController < ApplicationController
 
     def update
         if @enquiry.update(enquiry_params)
-            redirect_to root_path, success: "JSON data has been updated successfully!"
+            # Save the data to the JSON file after an existing record is updated
+            save_to_json
+            redirect_to root_path, success: "JSON data updated successfully!"
         else
-            render :edit
+            redirect_to root_path, danger: "JSON data is failed!"
         end
+    end
+
+    # Action to serve the JSON data
+    def json_data
+        json_data = File.read(json_file_path)
+        render json: json_data, content_type: 'application/json'
     end
     
     private
@@ -36,4 +65,21 @@ class EnquiriesController < ApplicationController
     def enquiry_params
         params.require(:enquiry).permit(:name, :email, :phone_number, :message)
     end
+
+    # Save form data to a JSON file
+    def save_to_json
+        # Retrieve all the records from your database
+        enquiries = Enquiry.all
+    
+        # Convert the records to an array of hashes
+        data = enquiries.map(&:attributes)
+    
+        # Write the data to the JSON file, overwriting its contents
+        File.write(json_file_path, JSON.pretty_generate(data))
+    end
+
+    def json_file_path
+        File.join(Rails.public_path, 'data', 'form_data.json')
+    end
+
 end
