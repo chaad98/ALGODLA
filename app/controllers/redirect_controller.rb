dcadc7c0c441a804cdde5ccc60782373b1201b96
chaad98 <faritzchad@yahoo.com>
2023-10-18 12:52:40 +0800

    Rechange the external link

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -1,5 +1,5 @@
 class RedirectController < ApplicationController
     def redirect_to_external
-        redirect_to "https://algodla.com/demo"
+        redirect_to "https://algodla.com/demo/index.htm"
     end
 end
