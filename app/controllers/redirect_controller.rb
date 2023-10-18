ea9c641edadac7da2b1234c30e4e11a27af545d7
chaad98 <faritzchad@yahoo.com>
2023-10-18 15:13:31 +0800

    Web server v7

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -1,5 +1,5 @@
 class RedirectController < ApplicationController
     def redirect_to_external
-        redirect_to "https://algodla.com/demo/index.htm" #, allow_other_host: true
+        redirect_to "http://demo.algodla.com/" #, allow_other_host: true
     end
 end
